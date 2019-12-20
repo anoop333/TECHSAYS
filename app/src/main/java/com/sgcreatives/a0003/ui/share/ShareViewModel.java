95dcad75c515c2e4379080d10ac32111a2ea4832
daines chacko <daineschacko05@gmail.com>
2019-12-20 21:39:21 +0530

    Initial commit

---
+++ app/src/main/java/com/sgcreatives/a0003/ui/share/ShareViewModel.java
@@ -0,0 +1,19 @@
+package com.sgcreatives.a0003.ui.share;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+public class ShareViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ShareViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is share fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
